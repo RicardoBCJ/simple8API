b1c7fa311e2edd3fd8dfd94dc53672fb444dfccb
Ricardo Castro <ricardoempessoal@gmail.com>
2020-12-26 14:50:41 -0300

    fixing cable at1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://redistogo-shallow-26994.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://simple8b.herokuapp.com', /http:\/\/simple8b.herokuapp.com.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://simple8f.herokuapp.com', /http:\/\/simple8f.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
