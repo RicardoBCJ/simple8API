ffb0efe9e7707c18d3428bc29cc89c2650267ea0
Ricardo Castro <ricardoempessoal@gmail.com>
2020-12-30 17:01:02 -0300

    image url added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,16 @@ class UsersController < ApplicationController
         end
     end
 
+
+    def update
+      user = User.find_by id:params[:id]
+      if user.update(user_params)
+        render json: user
+      else
+        render json: user_params, status: :unprocessable_entity
+      end
+    end  
+
     def create
         user = User.new(user_params)
         if user.save
