768e32faedbe7eae0f6c113f0e4b41a9e9f06146
Ricardo Castro <ricardoempessoal@gmail.com>
2020-12-30 14:48:36 -0300

    image url added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_174519) do
+ActiveRecord::Schema.define(version: 2020_12_30_174625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2020_12_23_174519) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "lastname"
+    t.string "imageurl"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
